7fa84edebf53450a77a1ba3f7d7143f0154fb395
Matias Karila <matias.karila@myy.haaga-helia.fi>
2023-01-24 10:08:16 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'; //as is
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'; // as is
+import { useState } from 'react'; // my import
+import React from 'react'; // my import
+
 
 export default function App() {
+
+  const [num1, setNum1] = useState(0)
+  const [num2, setNum2] = useState(0)
+  const [result, setResult] = useState(0);
+
+  const plus = () => {
+    var summa = Number(num1) + Number(num2);
+    setResult(summa);
+  }
+
+  const minus = () => {
+    var erotus = Number(num1) - Number(num2);
+    setResult(erotus);
+  }
+
+  const input1Changed = (event) => {
+    setNum1(event.target.value);
+  }
+
+  const input2Changed = (event) => {
+    setNum2(event.target.value);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text>Result: {result}</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setNum1}
+        value={num1}
+        keyboardType="numeric" />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setNum2}
+        value={num2}
+        keyboardType="numeric" />
+
+      <View style={styles.row}>
+
+        <Button
+            style={styles.button}
+            title="+"
+            color="#f194ff"
+            onPress={plus} />
+
+        <Button
+            style={styles.button}
+            title="-"
+            color="#f194ff"
+            onPress={minus} />
+      </View>
+
     </View>
   );
 }
@@ -17,4 +72,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  //my styles below
+  input: {
+    height: 30,
+    width: 150,
+    margin: 5,
+    borderWidth: 2,
+    padding: 5,
+  },
+  button: {
+    height: 20,
+    width: 20,
+    justifyContent: "center",
+    padding: 150,
+  },
+  row: {
+    flexDirection: "row"
+  }
+
 });
